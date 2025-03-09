14d8cc67de106b75a95b0f0f0b2fd64271fb1dc5
Abrahão Martins <56268335+abrahaosrmartins@users.noreply.github.com>
2025-03-09 13:53:22 -0300

    wip: componentizando o conteudo da página

--- vagas-front/src/components/layouts/Conteudo.vue
+++ vagas-front/src/components/layouts/Conteudo.vue
@@ -1,10 +1,21 @@
 <template>
     <div>
-        <p>Conteudo</p>
+        <home />
+        <publicar-vaga />
     </div>
 </template>
 
 <script>
+import Home from '../views/Home.vue';
+import PublicarVaga from '../views/PublicarVaga.vue';
+
+export default {
+    name: 'Conteudo',
+    components: {
+        Home,
+        PublicarVaga
+    }
+}
 </script>
 
 <style>
---
+++ vagas-front/src/components/views/PublicarVaga.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>Publicar Vaga</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Publicar Vaga'
+    }
+</script>
+
+<style></style>
