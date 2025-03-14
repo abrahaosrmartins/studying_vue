30c0bd91fa3775f1a2e317e240a8ea3bb36d63cb
Abrahão Martins <56268335+abrahaosrmartins@users.noreply.github.com>
2025-03-14 14:22:29 -0300

    Estudo de lifecyle hooks

--- vagas-front/src/components/layouts/Conteudo.vue
+++ vagas-front/src/components/layouts/Conteudo.vue
@@ -15,6 +15,48 @@ export default {
     components: {
         Home,
         PublicarVaga
+    },
+    methods: {
+        // code
+    },
+    beforeCreate() {
+        console.log('Antes de criar');
+    },
+    created() {
+        console.log('Criado');
+    },
+    beforeMount() {
+        console.log('Antes de montar');
+    },
+    mounted() {
+        console.log('Montado');
+    },
+    beforeUpdate() {
+        console.log('Antes de atualizar');
+    },
+    updated() {
+        console.log('Atualizado');
+    },
+    beforeUnmount() {
+        console.log('Antes de desmontar');
+    },
+    unmounted() {
+        console.log('Desmontado');
+    },
+    errorCaptured() {
+        console.log('Erro capturado');
+    },
+    renderTracked() {
+        console.log('Re-renderizado');
+    },
+    renderTriggered() {
+        console.log('Re-renderização acionada');
+    },
+    activated() {
+        console.log('Componente é ativado');
+    },
+    deactivated() {
+        console.log('Componente é desativado');
     }
 }
 </script>
