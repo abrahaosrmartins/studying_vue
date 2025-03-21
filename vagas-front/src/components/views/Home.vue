a2afedb89547566f1ae254a1303a97b19e948c67
Abrahão Martins <56268335+abrahaosrmartins@users.noreply.github.com>
2025-03-21 09:25:50 -0300

    componentizando as páginas

--- vagas-front/src/components/views/Home.vue
+++ vagas-front/src/components/views/Home.vue
@@ -1,50 +1,44 @@
 <template>
-    <div>
-        <h1>Componente Home</h1>
+    <div class="container py-4">
+        <div class="row">
+            <div class="col">
+                <pesquisar-vaga />
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-4">
+                <indicador titulo="Vagas abertas" indicador="25" bg="bg-dark" color="text-white" />
+            </div>
+            <div class="col-4">
+                <indicador titulo="Profissionais cadastrados" indicador="125" bg="bg-dark" color="text-white" />
+            </div>
+            <div class="col-4">
+                <indicador titulo="Visitantes online" :indicador="usuariosOnline" bg="bg-ligth" color="text-dark" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue';
+import Indicador from '@/components/comuns/Indicador.vue';
+
 export default {
     name: 'Home',
-    // beforeCreate() {
-    //     console.log('Antes de criar');
-    // },
-    // created() {
-    //     console.log('Criado');
-    // },
-    // beforeMount() {
-    //     console.log('Antes de montar');
-    // },
-    // mounted() {
-    //     console.log('Montado');
-    // },
-    // beforeUpdate() {
-    //     console.log('Antes de atualizar');
-    // },
-    // updated() {
-    //     console.log('Atualizado');
-    // },
-    beforeUnmount() {
-        console.log('Antes de desmontar');
+    components: {
+        PesquisarVaga,
+        Indicador
     },
-    unmounted() {
-        console.log('Desmontado');
+    data: () => ({
+        usuariosOnline: 0
+    }),
+    methods: {
+        getUsuariosOnline() {
+            this.usuariosOnline = Math.floor(Math.random() * 101) // entre 0 e 100
+        }
     },
-    // errorCaptured() {
-    //     console.log('Erro capturado');
-    // },
-    // renderTracked() {
-    //     console.log('Re-renderizado');
-    // },
-    // renderTriggered() {
-    //     console.log('Re-renderização acionada');
-    // },
-    activated() {
-        console.log('Componente é ativado');
-    },
-    deactivated() {
-        console.log('Componente é desativado');
+    created() {
+        setInterval(this.getUsuariosOnline, 1000) // a cada 1 segundo
     }
 }
 </script>
